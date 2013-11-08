2fdaa58b494bf9fa6f8ad376cacb9d1b89aa9920
Colin McFadden <colin@divergentmedia.com>
2013-11-08 15:19:53 -0600

    bitbox landing page

---
+++ application/controllers/home.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->load->view("home");
+	}
+
+}
+
+/* End of file  */
+/* Location: ./application/controllers/ */
---
+++ application/views/home.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>BitBox</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap CSS -->
+		<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet" media="screen">
+	</head>
+	<body class="container">
+		<!-- jQuery -->
+		<script src="//code.jquery.com/jquery.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+
+		<div class="jumbotron">
+			<div class="container">
+				<h1>Welcome to BitBox</h1>
+				<p>There's nothing you can do here, unless you have a download link.</p>
+				<p>
+					If you'd like your own BitBox, go to <a href="http://github.com/divergentmedia/bitbox">http://github.com/divergentmedia/bitbox</a>.
+				</p>
+			</div>
+		</div>
+
+	</body>
+</html>
+
